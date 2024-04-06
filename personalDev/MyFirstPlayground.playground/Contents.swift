1fb91e95f72c53409c25542987d55f40a2c09c9d
Nick Landry <nglandry@mines.edu>
2024-04-06 17:32:13 -0600

    Updated code syntax, functionality remains the same

--- personalDev/MyFirstPlayground.playground/Contents.swift
+++ personalDev/MyFirstPlayground.playground/Contents.swift
@@ -1,11 +1,9 @@
 import UIKit
 import Foundation
 
-// Get input from the user
 print("Enter a string:")
-if let input = readLine() {
+if let input = readLine(strippingNewline: true) {
     print("You entered: \(input)")
 } else {
     print("Error reading input.")
 }
-
